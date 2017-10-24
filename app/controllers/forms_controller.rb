8c51f590b8b9cf2061694cddc0f54459aac78fb0
Preston Matheson <pmphoto123@gmail.com>
2017-10-23 22:21:18 -0600

    stopping point for the night, currently have some of the form logic in the makes

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -2,6 +2,28 @@ class FormsController < ApplicationController
   def show
     
   end
+
+  def new
+    @form = Form.new
+  end
+
+  def create
+    @user_id = Form.new
+    if @user_id.save
+      format.html { redirect_to @user_id, notice: 'Your post is now live.' }
+    else
+      format.html { render :new }
+    end
+    # @blog = Blog.new(blog_params)
+
+    # respond_to do |format|
+    #   if @blog.save
+    #     format.html { redirect_to @blog, notice: 'Your post is now live.' }
+    #   else
+    #     format.html { render :new }
+    #   end
+    # end
+  end
   
   def account_activation_form
     
