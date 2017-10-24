5579e2ec8f35527c8c543b102b56503c72c25571
Preston Matheson <pmphoto123@gmail.com>
2017-10-23 21:35:54 -0600

    psudo coded the account acrivator class that will handle the http POST

---
+++ lib/account_activator.rb
@@ -0,0 +1,10 @@
+require 'httparty'
+class AccountActivator
+  def intialize
+    # @headers => some content
+  end
+
+  def activate_account(user_id)
+    # accept a single user_id and pass it into the url for account activation to the taxbot.
+  end
+end
